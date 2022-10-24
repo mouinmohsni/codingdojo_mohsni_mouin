437cf1f1e9b3106de98b437f04fdce2a5bc8fbb0
Mouin Mohsni <mouin.mohsni@gmail.com>
2022-10-24 19:11:39 +0100

    Assignment day !

---
+++ Python-Flask/Week1/Day1/practice-assignment/numbers.py
@@ -0,0 +1,29 @@
+# int_to_float = float(35)
+# float_to_int = int(44.9)
+# int_to_complex = complex(35)
+# print(int_to_float)
+# print(float_to_int)
+# print(int_to_complex)
+# print(type(int_to_float))
+# print(type(float_to_int))
+# print(type(int_to_complex))
+# first_name = "Zen"
+# last_name = "Coder"
+# age = 27
+# print(f"My name is {first_name} {last_name} and I am {age} years old.")
+# list ----------------------------------------------------------
+# fruits = ['apple', 'banana', 'orange', 'strawberry']
+# vegetables = ['lettuce', 'cucumber', 'carrots']
+# fruits_and_vegetables = fruits + vegetables
+# print(fruits_and_vegetables)
+# salad = 3 * vegetables
+# print(salad)
+x = [99,4,2,5,-3,6,10,-8]
+print(x[:])
+#the output would be [99,4,2,5,-3]
+print(x[1:])
+#the output would be [4,2,5,-3];
+print(x[:4])
+#the output would be [99,4,2,5]
+print(x[2:6])
+#the output would be [2,5];
