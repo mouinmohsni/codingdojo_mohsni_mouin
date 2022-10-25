d929cd5114bc3e8d1ea3ec974d01d72b7f831c9b
Mouin Mohsni <mouin.mohsni@gmail.com>
2022-10-25 15:44:54 +0100

    Affectation : Utilisateur update

--- Python-Flask/Week1/Day2/practice-assignment/Assignment_User/Assignment_user.py
+++ Python-Flask/Week1/Day2/practice-assignment/Assignment_User/Assignment_user.py
@@ -1,4 +1,4 @@
-from unicodedata import name
+
 
 
 class user:
@@ -24,16 +24,39 @@ class user:
         print(f"user : {self.first_name} {self.last_name} Balance: ${self.account_balance} sand monny to { other_user.first_name}  his balance now is { other_user.account_balance}")
 
 name_1 = user( "mouin" , "mohsni", "mouin.mohsni@" ,1000)
-print(name_1.first_name)
-#print(name_1)
+name_2 = user( "nassim" , "chaaben", "nassim.chaaben@" ,3000)
+name_3 = user( "amin" , "mlayah", "nassim.chaaben@",100 )
 
-name_1.make_withdrawal(100)
-
-print(name_1.account_balance)
+name_1.make_deposit(500)
+name_1.make_deposit(500)
+name_1.make_deposit(1500)
+name_1.make_withdrawal(500)
 name_1.display_user_balance()
 
-name_2 = user( "nassim" , "chaaben", "nassim.chaaben@" ,100)
+name_2.make_deposit(1000)
+name_2.make_withdrawal(900)
+name_2.display_user_balance()
+
+name_3.make_withdrawal(500)
+name_3.make_withdrawal(1000)
+name_3.make_withdrawal(2000)
+name_1.make_deposit(1500)
 
 name_1.transfer_money(name_2, 200)
+name_1.transfer_money(name_3, 450)
 
-print(name_2.account_balance)
+
+
+
+
+# print(name_1.first_name)
+
+
+# name_1.make_withdrawal(100)
+
+# print(name_1.account_balance)
+
+
+
+
+# print(name_2.account_balance)
