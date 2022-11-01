6173ddaa2e7beaba3ba0f15f76a7e4166bd6e824
Mouin Mohsni <mouin.mohsni@gmail.com>
2022-11-01 11:36:34 +0100

    update  Playground

--- Python-Flask/Week1/Day4/practice-assignment/Assignment_Playground/server.py
+++ Python-Flask/Week1/Day4/practice-assignment/Assignment_Playground/server.py
@@ -1,3 +1,4 @@
+from ast import Num
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -5,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html",num=1,color="green")
 
 
 @app.route('/play')
