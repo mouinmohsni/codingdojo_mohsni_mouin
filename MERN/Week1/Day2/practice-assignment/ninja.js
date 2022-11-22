8610ef7d6b9cec0a5860cdcfeca36ec64814a0f7
Mouin Mohsni <mouin.mohsni@gmail.com>
2022-11-22 13:46:06 +0100

    ninja

---
+++ MERN/Week1/Day2/practice-assignment/ninja.js
@@ -0,0 +1,31 @@
+class Ninja {
+    constructor(name, health =10, speed =3 ) {
+        this.name = name;
+        this.health = health;
+        this.speed = speed;
+        this.strength = 3;
+    }
+
+    sayName() {
+        console.log(this.name);
+        console.log(` my ninja name is ${this.constructor.name}`); // what a defferant about 
+        console.log(` my ninja name is ${this.name}`); // what a defferant about 
+    }
+    
+    showStats(){
+        console.log(` my ninja name is ${this.name} i have  like stats  ${this.health} in health ${this.speed} in speed and ${this.strength} n strength `);
+    }
+
+    drinkSake(){
+        console.log(' now i will drink Sake and you will see my power (*^◇^)_旦 (*^◇^)_旦 (*^◇^)_旦');
+        this.health += 10;
+        console.log(` my ninja name is ${this.name} i have  like stats  ${this.health} in health ${this.speed} in speed and ${this.strength} n strength `);
+
+
+}
+}
+
+const ninja1 = new Ninja("Hyabusa");
+ninja1.sayName();
+ninja1.showStats();
+ninja1.drinkSake();
