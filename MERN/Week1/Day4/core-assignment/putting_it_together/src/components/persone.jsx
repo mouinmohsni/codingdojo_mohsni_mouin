5064533b435d5251475443150c71ef127f11fdca
Mouin Mohsni <mouin.mohsni@gmail.com>
2022-11-24 20:13:24 +0100

    Putting it Together

---
+++ MERN/Week1/Day4/core-assignment/putting_it_together/src/components/persone.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+
+export default class Persone extends Component {
+    constructor(props) {
+        super(props)
+        this.state ={
+        age : this.props.age 
+        }
+    }
+            // {/*---------------- method  ti yous it in button ----------*/}
+    chageage(){
+        this.setState({
+        
+            age : this.state.age + 1 // using state here not props to add +1 in age 
+            
+        })
+
+    }
+    render() {
+    return (
+            <div>
+                
+                {/*---------------- model to chow him in pag ----------*/}
+                <h2> {this.props.firstname}{this.props.lastname} </h2>
+                 <p>age : {this.state.age}</p> {/*and youse it also to here to sow  */}
+                <p>Hair Color: {this.props.hairColor}</p>
+                <button onClick={() => { this.chageage() }}>Birthday Buutton for {this.props.lastname} {this.props.firstname}</button>
+
+
+
+
+
+
+            </div>
+        )
+    }
+}
