7a72abfb3fd1730d7cbf7bbbe773aec4748282e4
Mouin Mohsni <mouin.mohsni@gmail.com>
2022-11-24 20:17:38 +0100

    up date

--- MERN/Week1/Day4/core-assignment/putting_it_together/src/components/persone.jsx
+++ MERN/Week1/Day4/core-assignment/putting_it_together/src/components/persone.jsx
@@ -4,7 +4,7 @@ export default class Persone extends Component {
     constructor(props) {
         super(props)
         this.state ={
-        age : this.props.age 
+        age : this.props.age  // put the age form prors  in the  age in state
         }
     }
             // {/*---------------- method  ti yous it in button ----------*/}
@@ -24,12 +24,7 @@ export default class Persone extends Component {
                 <h2> {this.props.firstname}{this.props.lastname} </h2>
                  <p>age : {this.state.age}</p> {/*and youse it also to here to sow  */}
                 <p>Hair Color: {this.props.hairColor}</p>
-                <button onClick={() => { this.chageage() }}>Birthday Buutton for {this.props.lastname} {this.props.firstname}</button>
-
-
-
-
-
+                <button onClick={() => { this.chageage() }}>Birthday Buutton for {this.props.lastname} {this.props.firstname}</button> {/* ---------------------call the method------put the first and laste name in the button ------ */}
 
             </div>
         )
