93f2f71eb97e6acea8d6f699752166301aaac7cf
Mouin Mohsni <mouin.mohsni@gmail.com>
2022-12-19 15:09:59 +0100

    JAVA

---
+++ JAVA/Week1/Day1/practice-assignment/YourOwnJavaProgram/YourOwnJavaProgram.java
@@ -0,0 +1,9 @@
+public class YourOwnJavaProgram{
+    public static void main(String[] args){
+
+            System.out.println("May name is coding Dojo");
+            System.out.println("I am 100 years old");
+            System.out.println("My hometown is Burbank, CA");
+
+    }
+}
