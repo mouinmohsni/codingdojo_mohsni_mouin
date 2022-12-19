f9ef4680a33aa8dee1534aa084227a5de40ce53d
Mouin Mohsni <mouin.mohsni@gmail.com>
2022-12-19 19:05:39 +0100

    CafeJava

---
+++ JAVA/Week1/Day1/practice-assignment/CafeJAva/CafeJava.java
@@ -0,0 +1,64 @@
+public class CafeJava {
+    public static void main(String[] args) {
+        // APP VARIABLES
+        // Lines of text that will appear in the app. 
+        String generalGreeting = "Welcome to Cafe Java, ";
+        String pendingMessage = ", your order will be ready shortly";
+        String readyMessage = ", your order is ready";
+        String displayTotalMessage = "Your total is $";
+        
+        // Menu variables (add yours below)
+        double mochaPrice = 3.5;
+
+        int lattePrice = 5;
+
+        double cappuccinoPrice = 7.5;
+
+        double dripCoffeePrice = 2.5;
+
+
+    
+        // Customer name variables (add yours below)
+        String customer1 = "Cindhuri";
+
+        String customer2 = "Sam";
+
+        String customer3 = "Jimmy";
+
+        String customer4 = "Noah";
+
+    
+        // Order completions (add yours below)
+        boolean isReadyOrder1 = false;
+        boolean isReadyOrder2 = false;
+        boolean isReadyOrder3 = false;
+        boolean isReadyOrder4 = true;
+    
+        // APP INTERACTION SIMULATION (Add your code for the challenges below)
+        // Example:
+        System.out.println(generalGreeting + customer1); // Displays "Welcome to Cafe Java, Cindhuri"
+    	// ** Your customer interaction print statements will go here ** //
+
+        System.out.println(generalGreeting + customer2);
+
+        System.out.println(generalGreeting + customer3);
+        System.out.println(generalGreeting + customer4);
+
+        if(isReadyOrder4){
+            System.out.println( customer4 + readyMessage + displayTotalMessage + cappuccinoPrice);
+            isReadyOrder2 = true ;
+        }
+         if (isReadyOrder2){
+             System.out.println(customer2 + readyMessage + " is 2 lattes  " + displayTotalMessage + (2 * lattePrice));
+             isReadyOrder3 = true;
+        }
+
+         if (isReadyOrder3){
+             System.out.println(customer3 + readyMessage + " is 2 lattes  " + displayTotalMessage + (2 * lattePrice));
+             
+        }
+
+
+
+    }
+}
